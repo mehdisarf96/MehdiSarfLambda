0a41f0ab407fc80cb179a626165117a46475c74f
M.Mehdi <m.sfj9696@gmail.com>
2022-04-05 11:19:09 +0430

    Implement Runnable using Lambda.

---
+++ src/com/mehdisarf/lambda/runnableusinglambda/RunnableUsingLambda.java
@@ -0,0 +1,19 @@
+package com.mehdisarf.lambda.runnableusinglambda;
+
+public class RunnableUsingLambda {
+
+    public static void main(String[] args) {
+
+        // A classic case where u can use a Lambda rather than create an anonymous inner class.
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printed inside a anonymous inner class.");
+            }
+        });
+        myThread.run();
+
+        Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside a Lambda Exp."));
+        myLambdaThread.run();
+    }
+}
