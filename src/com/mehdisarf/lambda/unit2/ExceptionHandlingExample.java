374292119a26516906a57da6d4ac0fa1435dd366
M.Mehdi <m.sfj9696@gmail.com>
2022-04-08 12:43:37 +0430

    Wrap my lambda with another lambda that has try-catch (my own way)

--- src/com/mehdisarf/lambda/unit2/ExceptionHandlingExample.java
+++ src/com/mehdisarf/lambda/unit2/ExceptionHandlingExample.java
@@ -6,25 +6,33 @@ public class ExceptionHandlingExample {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 3, 4, 5, 6};
-        int key = 3;
+        int key = 0;
 
-        BiConsumer<Integer, Integer> consumer = (a, b) -> {
+        BiConsumer<Integer, Integer> consumer = (a, b) -> System.out.println(a / b);
+
+
+        WrapperWithException wrapper = (theConsumer, a, b) -> {
             try {
 
-                System.out.println(a / b);
+                theConsumer.accept(a, b);
 
             } catch (ArithmeticException e) {
-                System.out.println("An Arithmetic Exception happened.");
+                System.out.println(e);
             }
         };
 
-        process(numbers, key, consumer);
+        process(numbers, key, wrapper, consumer);
     }
 
-    public static void process(int[] numbers, int key, BiConsumer<Integer, Integer> biConsumer) {
+    public static void process(int[] numbers, int key, WrapperWithException wrapper, BiConsumer<Integer, Integer> biConsumer) {
         for (int i : numbers) {
-            biConsumer.accept(i, key);
+            wrapper.wrap(biConsumer, i, key);
         }
     }
+
 }
 
+
+interface WrapperWithException {
+    void wrap(BiConsumer<Integer, Integer> consumer, int a, int b);
+}
